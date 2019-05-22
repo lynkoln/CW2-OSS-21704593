f5f4ed33864091b272a67675ee38707cd91711a7
lynkoln <jakub.jonczyk30@gmail.com>
2019-05-22 14:26:12 +0100

    Task 3 Stage

---
+++ student.php
@@ -0,0 +1,58 @@
+<?php
+
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
+
+
+// check logged in
+if (isset($_SESSION['id'])) {
+
+    if(isset($_POST['student'])){
+      $students = $_POST['student'];
+      echo "<br>";
+      for($i = 0; $i < sizeof($students);$i++){
+
+        $query = "DELETE FROM `student` WHERE studentid={$students[$i]};";
+        if (!mysqli_query($conn, $query)) {
+             echo "Error deleting record: " . mysqli_error($conn);
+          }
+      }
+    }
+
+
+   echo template("templates/partials/header.php");
+   echo template("templates/partials/nav.php");
+
+   $sql = "select * from student;";
+   $result = mysqli_query($conn,$sql);
+
+
+   echo "<table>";
+   echo "<tr>";
+   echo "<th>*</th><th>Firstname</th> <th>Lastname</th> <th>House</th> <th>Town</th> <th>County</th> <th>Country</th> <th>Postcode</th>";
+   echo "<form action='student.php' method='post'>";
+   while($row = mysqli_fetch_assoc($result))
+   {
+     echo "<tr>";
+     echo "<td> <input type='checkbox' name='student[]' value='{$row['studentid']}'> </td>";
+     echo "<td>{$row['firstname']}</td>";
+     echo "<td>{$row['lastname']}</td>";
+     echo "<td>{$row['house']}</td>";
+     echo "<td>{$row['town']}</td>";
+     echo "<td>{$row['county']}</td>";
+     echo "<td>{$row['country']}</td>";
+     echo "<td>{$row['postcode']}</td>";
+     echo "</tr>";
+   }
+
+   echo "</table>";
+   echo "<input type='submit' value='Delete'>";
+    echo "</form>";
+
+
+}
+else{
+  echo "You do not have access to this place man. Please <a href='index.php'>login</a> or go away";
+}
+?>
---
+++ templates/partials/nav.php
@@ -0,0 +1,10 @@
+
+<div id="main-nav">
+
+   <a href="modules.php">My Modules</a> |
+   <a href="assignmodule.php">Assign Module</a> |
+   <a href="student.php">Students</a> |
+   <a href="details.php">My Details</a> |
+   <a href="logout.php">Logout</a>
+
+</div>
